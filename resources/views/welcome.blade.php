d39abc457b047370959cf1e7178525bdb2dbd8f5
Chetan Naik <chetan.n@vmokshagroup.com>
2023-02-14 16:15:45 +0530

    responsive issue updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -622,18 +622,18 @@
                 delay: 5000
             },
             // Media
-            // breakpoints: {
-            //   // when window width is >= 320px
-            //   320: {
-            //     slidesPerView: 2,
-            //     spaceBetween: 20
-            //   },
-            //   // when window width is >= 480px
-            //   480: {
-            //     slidesPerView: 4,
-            //     spaceBetween: 30
-            //   },
-            // }
+            breakpoints: {
+              // when window width is >= 320px
+              320: {
+                slidesPerView: 2,
+                spaceBetween: 20
+              },
+              // when window width is >= 480px
+              480: {
+                slidesPerView: 4,
+                spaceBetween: 30
+              },
+            }
         });
     </script>
 
